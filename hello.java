d3810619e2fb7c344fcab76ef263905c0c0b4672
2019wa86690 <137179162+2019wa86690@users.noreply.github.com>
2023-11-25 12:04:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World of Ankush! Part 2 for making some changes"); 
+        System.out.println("Made changes for webhook"); 
     }
 }
